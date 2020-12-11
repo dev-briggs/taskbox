6f167d3ba923396f978df5550a1dca24db3ac6cc
Briggs <dev_briggs_victoria@bitbucket.org>
2020-12-11 07:43:06 +0000

    taskbox UI

---
+++ src/stories/Header.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Header } from "./Header";
+
+export default {
+  title: "Example/Header",
+  component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {},
+  onLogin: () => {},
+  onLogout: () => {},
+  onCreateAccount: () => {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = { ...LoggedIn.args };
